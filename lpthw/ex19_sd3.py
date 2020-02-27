1abf1c70edb38103c24354db0d4b3326972fd329
briesenberg07 <ries07@uw.edu>
2020-02-27 12:09:52 -0800

    lpthw

--- lpthw/ex19_sd3.py
+++ lpthw/ex19_sd3.py
@@ -3,15 +3,15 @@ script, aw1, aw2 = argv
 
 def word_meld(w1, w2):
     print("This function melds words together!")
-    print(f"Prepare to see an entirely new word, made from the words you provided, namely '{w1}' and '{w2}'.")
+    print(f"Prepare to see an entirely new word created from two inputs, '{w1}' and '{w2}'.")
     print(f"Here it is!\n{w2}{w1}")
     print("I hope you weren't too hung up on the order of the words you provided, man. Too bad.")
-
+# Method 1: Integers
 print("I wanna see first if this will work with integers.\nLet's call this 'number-word'.")
 word_meld(5, 7)
 print("I *think* I just printed '75'.")
 print("\n")
-
+# Method 2: argv
 print("Now we try it using 'argv'.")
 # *if* I used w1 and w2 for argv second and third values above and in script below...
 # ...there wouldn't be any conflict between w1 and w2 from the CLI and w1 and w2 in the function definition.
@@ -25,7 +25,7 @@ First, the third argument you entered on the command line. That is, '{aw2}'.
 Second, the second argument you entered on the command line. That is, '{aw1}'.
 Note that the first thing you entered in the command line prompt, that is, the script name '{script}', wasn't used as part of the new word.
 """)
-
+# Method 3: argv + input
 print("Now let's combine what was given on the command line and what you'll give in input right here.")
 print("I'll ask you for a couple of pieces of input, I1 and I2.")
 I1 = input("I1: ")
@@ -40,4 +40,10 @@ Lastly, I'll combine UBER1 ('{UBER1}') and UBER2 ('{UBER2}') to make a new word!
 But not in the order you'd think! At any stage of the process!
 """)
 word_meld(UBER1, UBER2)
-# SQ3 says call the function *10* ways. So far I've called it 3 ways...
+# Method 4: Number input, converted to integers, plus math!
+print("Please input two numbers when prompted. Input only one or more characters 0-9--no spaces or other characters.")
+n1 = int(input("First number: "))
+n2 = int(input("Second number: "))
+n1math = n1 + 1
+n2math = n2 + 2
+word_meld(n1math, n2math)
--- lpthw/ex20.py
+++ lpthw/ex20.py
@@ -10,21 +10,23 @@ script, input_file = argv
 # * WHAT IS F??? Seems that it is shorthand for "file"--but would a letter other than f work?? I believe that it would.
 
 # We define a function called "print_all" with one argument, "f"*
-def print_all(f):
+    # I CHANGED THE Fs TO Xs because I wanted to know whether somehow the var name "f" was set...somewhere else, or whether it could be any name.
+    # Per LPTHW, "The f is a variable just like you had in other functions [...], except this time it's a file."
+def print_all(x):
     # NOW IS WHERE IT GETS TO STUFF I DON'T QUITE KNOW YET
     # We are using a method or function with "f"--read. Once we have read f, we print the result to standard output.
-    print(f.read())
+    print(x.read(), end = "")
 
 # Define "rewind" with one argument "f"*
-def rewind(f):
+def rewind(x):
     # This function will seek ("to change the file object's position") the 1st byte in the file (go to the byte with an offset of 0)
-    f.seek(0)
+    x.seek(0)
 
 # Define "print_a_line" with two args: line_count, and--again--"f"*
-def print_a_line(line_count, f):
+def print_a_line(line_count, x):
     # readline ("reads a single line from the file") of "f", and prints this out preceeded by the value of var line_count
     # NOTE AGAIN that what is called "line_count" here could be called something else later--in fact the var name used for this function later will be "current_line"
-    print(line_count, f.readline())
+    print(line_count, x.readline(), end = "")
 
 # Opens the input_file--in my language, I'd say that this opened file becomes the value of the var current_file
 current_file = open(input_file)
@@ -48,13 +50,17 @@ print("Let's print three lines:")
 # In the 3 lines of code below that start with "current_line," we are iteratively re-defining the current_line variable
 # Here it is 1, next it will be 1 (value at the start of that line of code) plus 1, next it will be 2 (value at the start of that line of code) plus one...
 current_line = 1
-# My question about this is to confirm that readline "marks its place" at the end of each line it reads, starting again at the beginning of the next line. Based on documentation at link above, it *seems* that it does this, but it isn't really explicit
+# My question about this is to confirm that readline "marks its place" at the end of each line it reads, starting again at the beginning of the next line.
+    # Based on documentation at link above, it *seems* that it does this, but it isn't really explicit
+    # Also see author's comments in ex20
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1
+# Previously: current_line = current_line +1
+    # But now, per SD 5, using += ("addition assignment") instead
+current_line += 1
 print_a_line(current_line, current_file)
 
-current_line = current_line + 1
+current_line += 1
 print_a_line(current_line, current_file)
 
 # SQ 2: "Write out what current_line is equal to on each function call [did this above] and trace how it becomes line_count in print_a_line"
