f60525bb17bc7eedbdfff5c1a54f677858265c69
Ben Riesenberg <ries07@uw.edu>
2020-05-12 14:46:31 -0700

    Thanks again @alexiskeely

--- python/rdfLibSer.py
+++ python/rdfLibSer.py
@@ -2,12 +2,12 @@
 """
 Created on Mon Nov 25 12:19:43 2019
 @author: alexis mcclimans
-Modified by Ben Riesenberg
 """
 
 import os
 import rdflib
 from rdflib import *
+import datetime
 
 """Generate directories - program will generate a set of directories to save the exported records in"""
 if not os.path.exists('export_xml'):# creates xml directory
@@ -27,13 +27,13 @@ URIS = [] # list for record uris
 for o in G.objects(subject=None,predicate=LDP.contains):#records are described in the parent repo using ldp.contains
     URIS.append(o)
 
-"""For each URI in the UW trellis repo, downloads the graph, and serializes and exports in ttl and xml"""
-print("...\nsaving downloaded graphs locally", flush=True)
+# For each URI in the UW trellis repo, downloads the graph, and serializes and exports in ttl and xml
+"""print("...\nsaving downloaded graphs locally", flush=True)
 for uri in URIS:
     label = uri.split('/')[-1] #useURI element as name
     g = Graph()
     g.load(uri,format='turtle')
-    g.serialize(destination='export_xml/'+label+'.rdf', format='xml')#creates xml serialization
+    g.serialize(destination='export_xml/'+label+'.rdf', format='xml')""" #creates xml serialization
     ## to add more serialization options, create a folder for holding the exports, and update the function args
     #g.serialize(destination='exports/NEW_EXPORT_FOLDER/'+label+'.FILE_TYPE', format='RDFLIB FORMAT')
 
@@ -41,3 +41,6 @@ for uri in URIS:
 g = Graph()
 for uri in URIS:
     g.parse(uri)
+
+print("\nsaving the graph")
+g.serialize(destination='export_xml/'+str(datetime.date.today())+'.rdf', format='xml')
