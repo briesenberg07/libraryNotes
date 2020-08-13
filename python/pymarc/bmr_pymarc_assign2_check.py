360e6833e7d297d4e71cc549823c193743b455de
briesenberg07 <ries07@uw.edu>
2020-08-13 11:42:09 -0700

    still confused; need to learn more about merging; restoring files

---
+++ python/pymarc/bmr_pymarc_assign2_check.py
@@ -0,0 +1,9 @@
+# Import all functions from the PyMARC library
+from pymarc import *
+
+# ... I've never used 'with' before!
+with open('newMarcfile.mrc', 'rb') as openFile:
+    mrFile = MARCReader(openFile)
+    for record in mrFile:
+        print(record['100'])
+        print(record['655'])
